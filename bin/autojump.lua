imlijh/autojump

commit 3e5cec67b995c61e8724ba8fe28cb0dd3469cd4c

Autojump lua script portability fix

bin/autojump.lua
@@ -1,13 +1,15 @@
+local AUTOJUMP_DIR = debug.getinfo(1, "S").source:match[[^@?(.*[\/])[^\/]-$]] .. "..\\AutoJump"
+local AUTOJUMP_BIN_DIR = AUTOJUMP_DIR .. "\\bin"
 local AUTOJUMP_BIN = (AUTOJUMP_BIN_DIR or clink.get_env("LOCALAPPDATA") .. "\\autojump\\bin") .. "\\autojump"
 
 function autojump_add_to_database() 
-  os.execute("python " .. AUTOJUMP_BIN .. " --add " .. clink.get_cwd() .. " 2> " .. clink.get_env("TEMP") .. "\\autojump_error.txt")
+  os.execute("python " .. "\"" .. AUTOJUMP_BIN .. "\"" .. " --add " .. "\"" .. clink.get_cwd() .. "\"" .. " 2> " .. clink.get_env("TEMP") .. "\\autojump_error.txt")
 end
 
 clink.prompt.register_filter(autojump_add_to_database, 99)
 
 function autojump_completion(word)
-  for line in io.popen("python " .. AUTOJUMP_BIN .. " --complete " .. word):lines() do
+  for line in io.popen("python " .. "\"" .. AUTOJUMP_BIN .. "\"" ..  " --complete " .. word):lines() do
     clink.add_match(line)
   end
   return {} 

commit 41ddfbdd2cc7bc9816f4700cb5e56ceb1526550d

fix(lua): fix broken portability
This issue is introduced by 3e5cec67. AUTO_JUMP_BIN_DIR is set by
install.py when given custom install arguments.

bin/autojump.lua
@@ -1,5 +1,3 @@
-local AUTOJUMP_DIR = debug.getinfo(1, "S").source:match[[^@?(.*[\/])[^\/]-$]] .. "..\\AutoJump"
-local AUTOJUMP_BIN_DIR = AUTOJUMP_DIR .. "\\bin"
 local AUTOJUMP_BIN = (AUTOJUMP_BIN_DIR or clink.get_env("LOCALAPPDATA") .. "\\autojump\\bin") .. "\\autojump"
 
 function autojump_add_to_database()