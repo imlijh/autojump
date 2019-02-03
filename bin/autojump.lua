8ce4297ea038dd96a7cf2e6c857d1753cde5264a
LI Jianhua <lay.79523@gmail.com>
2019-02-03 14:35:24 +0800

    perf(lua): accelerate the execution of cwd injection function

--- bin/autojump.lua
+++ bin/autojump.lua
@@ -1,12 +1,21 @@
 local AUTOJUMP_BIN = (AUTOJUMP_BIN_DIR or clink.get_env("LOCALAPPDATA") .. "\\autojump\\bin") .. "\\autojump"
 
-function autojump_add_to_database()
-  os.execute("python " .. "\"" .. AUTOJUMP_BIN .. "\"" .. " --add " .. "\"" .. clink.get_cwd() .. "\"" .. " 2> " .. clink.get_env("TEMP") .. "\\autojump_error.txt")
+local function autojump_add_to_database()
+  -- Start a python process in background and return without waiting for
+  -- subprocess to exit.
+  -- Use "%TEMP%/autojump_error.txt" as a lock file.
+  local cmdline = string.format(
+    [[(start "" /b python "%s" --add "%s" 2> "%s") 2>nul]],
+    AUTOJUMP_BIN,
+    clink.get_cwd(),
+    clink.get_env("TEMP") .. "\\autojump_error.txt"
+  )
+  os.execute(cmdline)
 end
 
 clink.prompt.register_filter(autojump_add_to_database, 99)
 
-function autojump_completion(word)
+local function autojump_completion(word)
   for line in io.popen("python " .. "\"" .. AUTOJUMP_BIN .. "\"" ..  " --complete " .. word):lines() do
     clink.add_match(line)
   end
